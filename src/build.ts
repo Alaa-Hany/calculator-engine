7155bbe34d375d147658869c9c95d848808ad434
Alaa Elsaid <elboray.alaa1@gmail.com>
2022-08-30 23:22:15 +0200

    modify some internal names

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,6 @@
 import { Calculator } from "./Calculator";
 if (typeof window === 'undefined'){
+    // @ts-ignore
     global.Calculator = Calculator;
 }else{
     (window as any).Calculator = Calculator;
